e8130b3ee167fbfec4bbb55e066a99329c84349c
Shreyas Bhat Kera <sbk67@cornell.edu>
2022-12-07 15:21:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ intents.message_content = True
 client = discord.Client(intents = intents)
 
 global control
-control = True
+control = False
 
 global counter
 global phase 
@@ -14,32 +14,63 @@ global end_experiment
 
 phase = 'Lesson'
 counter = 0
-scoring_results = 'You got 5/6...'
-end_experiment = 'Done with experiment'
+scoring_results = 'Congrats! You got 4/5 questions correct. Your answers to the question concerning the Civil War, Revolutionary War, US Space history and Boston Tea Party were correct. The bot provided useful facts for answering these questions. Therefore, this bot performed extremely well.'
+end_experiment = 'Done with experiment, thank you for participating!'
 
 lessons = [
-  'lesson1',
-  'lesson2',
-  'lesson3',
-  'lesson4',
-  'lesson5',
-  'lesson6'
+  '''Thank you for participating. Today, you  will be working with this bot. The bot will begin by providing you with a factafter which you should indicate how familiar you are with it. The more you know about the topic associated with a fact, the less likely you will  be to receive additional facts on that topic. You will receive a total of 6 facts, from a list of 100 possible facts. After this there will be 5 MCQ's, randomly chosen from a set of 100 questions. 
+  
+  At the height of the Ice Age, much of the world's water was locked up in vast continental ice sheets. As a result, the Bering Sea was hundreds of meters below its current level, and a land bridge, known as Beringia, emerged between Asia and North America. 
+  Please type how familiar you were about this fact on a  scale of 1-3.''',
+  
+  '''The powerful East India Company, finding itself in critical financial straits, appealed to the British government in 1773, which granted it a monopoly on all tea exported to the colonies. Please type how familiar you were about this fact on a  scale of 1-3.''',
+  
+  '''Samuel Adams boarded three British ships lying at anchor and dumped their tea cargo into Boston harbor during the Boston Tea Party. Please type how familiar you were about this fact on a  scale of 1-3.''',
+  
+  '''George Washington was unanimously chosen president and took the oath of office at his inauguration on April 30, 1789. Please type how familiar you were about this fact on a  scale of 1-3.''',
+  
+  '''President Lincoln delivered the 272 word Gettysburg Address on November 19, 1863 on the battlefield near Gettysburg, Pennsylvania. Please type how familiar you were about this fact on a  scale of 1-3.''',
+  
+  '''With Project Mercury in 1962, John Glenn became the first U.S. astronaut to orbit the Earth. Please type how familiar you were about this fact on a  scale of 1-3.'''
 ]
 
 tests = [
-  'test1',
-  'test2',
-  'test3',
-  'test4',
-  'test5'
+  '''Please answer the following questions by typing a, b, or c.
+  
+  Who was the first U.S. astronaut to orbit the earth, in 1962?
+  a - Neil Armstrong
+  b - John Glenn
+  c - Buzz Aldrin''',
+  
+  '''Who was the first president of the United States?
+  a - George Washington
+  b - Abraham Lincoln
+  c - Donald Trump''',
+  
+  '''What was President Lincoln's famous speech at Pennsylvania called?
+  a - Gettysburg Address
+  b - Infamy Speech
+  c - I Have a Dream''',
+  
+  '''Who was President during Project Mercury?
+  a - John F Kennedy
+  b - Dwight D. Eisenhower
+  c - Richard Nixon''',
+  
+  '''Which individual was associated with Boston Tea Party?
+  a - George Washington
+  b - Samuel Adams
+  c - Martin Luther King Jr'''
 ]
 
 questions = [
-  'question1',
-  'question2',
-  'question3',
-  'question4',
-  'question5'
+  '''For each of the following adjectives, please indicate how well it describes  the tutoring session given by the bot on a scale of 1-10.
+  
+  Analytical.''',
+  'Enjoyable.',
+  'Polite.',
+  'Likable',
+  'Informative.'
 ]
 
 def lesson(counter):
@@ -97,5 +128,5 @@ async def on_message(message):
     else:
       await message.channel.send(question(counter))
       counter = counter + 1
-    
+
 client.run(os.getenv('TOKEN'))
